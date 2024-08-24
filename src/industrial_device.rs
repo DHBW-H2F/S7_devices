fc30979347ff0af91d82c8242ac7596b76fcb2f6
leo <leo.lemaire007@orange.fr>
2024-08-24 11:45:53 +0200

    IndustrialDeviceError

--- src/industrial_device.rs
+++ src/industrial_device.rs
@@ -1,22 +1,42 @@
 use std::collections::HashMap;
 
+use crate::errors::S7Error;
 use crate::s7_connexion::S7Connexion;
 use crate::S7Device;
 
+use industrial_device::errors::IndustrialDeviceError;
 use industrial_device::types::Value;
 use industrial_device::IndustrialDevice;
 
 impl IndustrialDevice for S7Device {
-    async fn connect(&mut self) -> Result<(), Box<dyn std::error::Error + Send>> {
-        match S7Connexion::connect(self).await {
-            Ok(_) => Ok(()),
-            Err(err) => Err(Box::new(err)),
-        }
+    async fn connect(&mut self) -> Result<(), IndustrialDeviceError> {
+        S7Connexion::connect(self).await?;
+        Ok(())
     }
 
-    async fn dump_registers(
-        &mut self,
-    ) -> Result<HashMap<String, Value>, Box<dyn std::error::Error + Send>> {
+    async fn dump_registers(&mut self) -> Result<HashMap<String, Value>, IndustrialDeviceError> {
         todo!()
+    }
+}
+
+impl From<S7Error> for IndustrialDeviceError {
+    fn from(value: S7Error) -> Self {
+        match value {
+            S7Error::S7ClientError { err } => {
+                IndustrialDeviceError::RequestError { err: Box::new(err) }
+            }
+            S7Error::DeviceNotConnectedError => IndustrialDeviceError::DeviceNotConnectedError {
+                err: Box::new(value),
+            },
+            S7Error::MismatchedRegisterLengthError => IndustrialDeviceError::RequestError {
+                err: Box::new(value),
+            },
+            S7Error::RegisterDoesNotExistsError => IndustrialDeviceError::RequestError {
+                err: Box::new(value),
+            },
+            S7Error::InvalidRegisterValue => IndustrialDeviceError::RequestError {
+                err: Box::new(value),
+            },
+        }
     }
 }
